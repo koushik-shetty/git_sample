ec790e9bbe9dcc08f5880b431f954cbaccbfc6e1
koushik-shetty <kou4307@gmail.com>
2015-08-07 21:08:46 +0530

    second commit

--- firstFile.cc
+++ firstFile.cc
@@ -5,7 +5,7 @@ int main(int cnt, char* args[])
 {
 	if(cnt > 1){
 		std::cout << cnt <<" params were passed and they are : \n";
-		std::for_each(std::begin(args),std::end(args+cnt),
+		std::for_each(args,args+cnt,
 			[](decltype(args[0]) &param){
 										std::cout <<"lambda\n";
 										std::cout << param;
